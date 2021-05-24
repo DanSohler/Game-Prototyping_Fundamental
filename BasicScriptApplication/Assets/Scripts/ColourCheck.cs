5183f0630d3ed665d69c9318c9622bec5a42b8df
Da <daniel.sohler73@gmail.com>
2021-05-24 16:57:15 +1000

    End of day push
    Updated word docs. Added BasicScriptApplicationbuild.

---
+++ BasicScriptApplication/Assets/Scripts/ColourCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourCheck : MonoBehaviour
+{
+
+	public GameObject thingToRemove;
+
+	//public float variables 
+
+	public float Red = 1.0f;
+	public float Green = 0.0f;
+	public float Blue = 0.0f;
+
+
+	//when an object enters this trigger object call theis function
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			if (other.GetComponent<Renderer>().material.color.b == Blue)
+			{
+				if (other.GetComponent<Renderer>().material.color.r == Red)
+				{
+					if (other.GetComponent<Renderer>().material.color.g == Green)
+					{
+						thingToRemove.gameObject.SetActive(false);
+					}
+				}
+			}
+		}
+	}
+
+}
---
+++ BasicScriptApplication/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Teleporter : MonoBehaviour
+{
+
+	public GameObject teleportOutLocation;
+	// public Material wantedMaterial;
+
+	//public float variables 
+
+	public float Red = 0.0f;
+	public float Green = 0.0f;
+	public float Blue = 0.0f;
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+		//Check if Teleport out location has been set.
+		if (teleportOutLocation == null)
+		{
+			Debug.LogWarning("No Teleporter Out Location has been set for " + transform.name + ", Please set it and restart");
+		}
+
+		//Checks to see if you have set the collider on this object to a trigger 
+		if (!GetComponent<Collider>().isTrigger)
+		{
+			Debug.LogWarning("Collider on " + transform.name + " is not set to \"Is Trigger\", please update and restart");
+		}
+	}
+
+	// This function is called then any collider touches the collider on this object.
+	void OnTriggerEnter(Collider collidedObject)
+	{
+		Debug.Log("Player Teleported");
+		//Teleport GameObject that was collided with to the teleportOutLocation
+		if (collidedObject.gameObject.tag == "Player")
+		{
+			if (collidedObject.GetComponent<Renderer>().material.color.b == Blue)
+			{
+				if (collidedObject.GetComponent<Renderer>().material.color.r == Red)
+				{
+					if (collidedObject.GetComponent<Renderer>().material.color.g == Green)
+					{
+						collidedObject.transform.position = teleportOutLocation.transform.position;
+					}
+
+
+				}
+
+			}
+		}
+
+		//This section Draws a line from the teleporter in to the teleporter out within the editor
+		void OnDrawGizmos()
+		{
+			Gizmos.DrawLine(transform.position, teleportOutLocation.transform.position);
+		}
+
+
+
+
+
+
+
+
+	}
+}
