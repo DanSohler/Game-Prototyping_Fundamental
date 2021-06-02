62406ee72fb41708aa02cfc31c3aac886fc3fa48
Dan Sohler <daniel.sohler73@gmail.com>
2021-06-02 16:48:57 +1000

    End of day push
    Added slingshot scripts

---
+++ Slingshot Neccesary Scripts/LineTrajectory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineTrajectory : MonoBehaviour
+{
+    public LineRenderer lr;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    public void RenderLine(Vector3 mouseStartPoint, Vector3 mouseEndPoint)
+    {
+        lr.positionCount = 2;
+        Vector3[] points = new Vector3[2];
+        points[0] = mouseStartPoint;
+        points[1] = mouseEndPoint;
+
+        lr.SetPositions(points);
+    }
+
+    public void EndLine()
+    {
+        lr.positionCount = 0;
+    }
+
+}
