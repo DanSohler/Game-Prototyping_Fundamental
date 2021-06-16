20c12bf428a71f2ad930398a4cef0971de262cf9
Daniel Sohler <daniel.sohler73@gmail.com>
2021-06-16 15:52:05 +1000

    End of Day Push No.8
    Added 'finesse force', direction and distance impacts overall push force. Updated TDD, finish it regardless.

--- SlingshotTest2/Assets/Scripts/PlayerController.cs
+++ SlingshotTest2/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,7 +15,9 @@ public class PlayerController : MonoBehaviour
     bool slingshotFired = false; // short bool that initiates a GUI component
     public LineTrajectory lineTraj; // Calls other script here for line rendering
 
-    
+    float addedDirectionalForceX; // Value for storing a float 
+    float addedDirectionalForceY; // Value for storing a float 
+    float addedDirectionalForce; // Value for storing a float 
 
     public float minSlingDistance; // min required distance to draw line + allow the playr to launch themselves
     public float maxSlingDistance; // max required distance to draw line + allow the playr to launch themselves
@@ -24,10 +27,13 @@ public class PlayerController : MonoBehaviour
     Vector3 mouseEndPoint; //tracks where mouse is no longer held down
     Vector3 launchDirection; // Impulse added to player character
 
+    // Energy meter variables
+    public Slider energyMeter;
+
     private void Start()
     {
         // calls line trajectory script
-        lineTraj = GetComponent<LineTrajectory>(); 
+        lineTraj = GetComponent<LineTrajectory>();
     }
 
     public void Update()
@@ -37,6 +43,8 @@ public class PlayerController : MonoBehaviour
         // Tracks players position, used for determining sling direction + line draw
         playerPosition = playerCharacter.gameObject.transform.position;
 
+
+        // Min/Max launch distance code
         // Minimum distance player can launch themselves  too
         if (Vector3.Distance(currentMousePosition, playerPosition) < minSlingDistance)
         {
@@ -54,22 +62,23 @@ public class PlayerController : MonoBehaviour
             canSling = true;
         }
 
+
+        // If its true, make the line blue
+        // If its false, make the line red
         if (canSling == true)
         {
-            // If its true, make the line blue
             lineTraj.wantedLineRenderer.startColor = new Color(255, 255, 255);
             lineTraj.wantedLineRenderer.endColor = new Color(0, 122, 255);
         }
         else
         {
-            // If its false, make the line red
             lineTraj.wantedLineRenderer.startColor = new Color(255, 255, 255);
             lineTraj.wantedLineRenderer.endColor = new Color(255, 10, 0);
         }
 
+        // While mouse is held down, render line between player position and currentMousePositions
         if (Input.GetMouseButton(0))
         {
-            // While mouse is held down, render line between player position and currentMousePositions
             lineTraj.RenderLine(playerPosition, currentMousePosition);
         }
 
@@ -83,9 +92,27 @@ public class PlayerController : MonoBehaviour
             // minuses the mouseEndPoint by the playerPosition, then normalizes to return a smooth number that isn't a bloated vector
             launchDirection = (mouseEndPoint - playerPosition).normalized;
 
+            // multiplies the force that will be pushing the player by the x and y position of both the player and mouse position, by a flat value.
+            addedDirectionalForceX = mouseEndPoint.x - playerPosition.x;
+
+            if (addedDirectionalForceX <= 0)
+            {
+                addedDirectionalForceX = addedDirectionalForceX * -1;
+            }
+
+            addedDirectionalForceY = mouseEndPoint.y - playerPosition.y;
+            if (addedDirectionalForceY <= 0)
+            {
+                addedDirectionalForceY = addedDirectionalForceY * -1;
+            }
+
+            addedDirectionalForce = (addedDirectionalForceX + addedDirectionalForceY) * power;
+
             if (canSling == true)
             {
-                playerCharacter.AddForce(launchDirection * power, ForceMode.Impulse);
+                playerCharacter.AddForce(launchDirection * addedDirectionalForce, ForceMode.Impulse);
+
+                Debug.Log("addedDirectionalForce = " + addedDirectionalForce);
 
                 Debug.Log("launchDirection = " + launchDirection);
                 slingshotFired = true;
@@ -94,10 +121,5 @@ public class PlayerController : MonoBehaviour
             lineTraj.EndLine();
             slingshotFired = false;
         }
-    }
-
-    public void FixedUpdate()
-    {
-        
     }
 }
